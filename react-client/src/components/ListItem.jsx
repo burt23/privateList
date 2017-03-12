6f5b69cbc148b53f52b70db8cfbfb3b02b73050d
burt23 <mail4mdb@gmail.com>
2017-03-12 13:57:05 -0700

    Added Timestamp to list items

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import TimeAgo from 'react-timeago';
 
-const ListItem = (props) => (
+
+
+const ListItem = (props) => {
+
+var time = props.item.time;
+
+
+return (
   <div className='item' >
 
-    <h4>{ props.item.name }</h4>
     <p>{ props.item.message }</p>
+    <TimeAgo className='timeStamp' date={time} />
   </div>
-)
+  )
+}
 
 export default ListItem;
