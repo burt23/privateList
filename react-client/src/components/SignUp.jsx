9f512261166a892eae9fac4e041823f31b54f0c5
burt23 <mail4mdb@gmail.com>
2017-03-11 16:02:29 -0800

    Added eventHandlers for signup input boxes

--- react-client/src/components/SignUp.jsx
+++ react-client/src/components/SignUp.jsx
@@ -5,14 +5,44 @@ class SignUp extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      username: '',
+      password: '',
+      verifier: '',
       userWantsLogin: false
     }
   //BIND FUNCTIONS
     this.handleSignUpChange = this.handleSignUpChange.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeVerifier = this.handleChangeVerifier.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  handleSubmit(event){
+    console.log('handlin submit bbabby');
+    event.preventDefault();
+  }
+
   handleSignUpChange(event){
     console.log('inside sign up');
     this.setState({ userWantsLogin: !this.state.userWantsLogin });
+  }
+
+  handleChangeUsername(event){
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleChangePassword(event){
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleChangeVerifier(event){
+    this.setState({
+      verifier: event.target.value
+    })
   }
   //DECLARE FUNCTIONS
     //HANDLE CHANGE, HANDLE SUBMIT
@@ -25,15 +55,15 @@ class SignUp extends React.Component {
         <div className = 'formWrapper'>
           <h1 id='mainTitle'>Private List</h1>
           <h3> Sign Up </h3>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className='signupBoxUser'>
-                <input type='text' id='username' placeholder='username' />
+                <input type='text' id='username' placeholder='username' onChange={this.handleChangeUsername} value={this.state.username}/>
               </div>
               <div className='signupBoxPass'>
-                <input type='text' id='password' placeholder='password' />
+                <input type='text' id='password' placeholder='password' onChange={this.handleChangePassword} value={this.state.password} />
               </div>
               <div className='signupBoxPass'>
-                <input type='text' id='passwordVerify' placeholder='password' />
+                <input type='text' id='passwordVerify' placeholder='password' onChange={this.handleChangeVerifier} value={this.state.verifier}/>
               </div>
               <div className='submitButton'>
                 <input type='submit' value='Submit' />
